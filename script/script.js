b26a22a286a5c0dd78d56d573595dfeff2c5faf6
Aleksey8205 <Aleksey8205@yandex.ru>
2025-01-18 01:27:58 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
 const animalImages = {
-    'animal1': 'image/broiler-calculator.png',
-    'animal2': 'image/rabbit-calculator.jpg',
-    'animal3': 'image/pig.webp',
-    'animal4': 'image/cow.webp',
-    'animal5': 'image/chicken.webp',
-    'animal6': 'image/peperel.webp',
-    'animal7': 'image/gobbler.webp',
-    'animal8': 'image/duck.jpg'
+    'animal1': 'image/broiler-calculator.png', //broiler
+    'animal2': 'image/rabbit-calculator.jpg', //rabbit  
+    'animal3': 'image/pig.webp',  //pig
+    'animal4': 'image/cow.webp', //cow
+    'animal5': 'image/chicken.webp', //chicken
+    'animal6': 'image/peperel.webp', //perepel
+    'animal7': 'image/gobbler.webp', //gobbler
+    'animal8': 'image/duck.jpg' //duck
 };
 
 // Цвета для каждого животного
 const animalColors = {
-    'animal1': 'rgb(153, 225, 239)', // Красный
-    'animal2': '#00ff00', // Зеленый
-    'animal3': '#0000ff', // Синий
-    'animal4': '#ffff00', // Желтый
-    'animal5': '#ffa500', // Оранжевый
-    'animal6': '#800080', // Пурпурный
-    'animal7': '#008080', // Бирюзовый
-    'animal8': '#808080'  // Серый
+    'animal1': 'rgb(153, 225, 239)', 
+    'animal2': 'rgb(127, 90, 154)', 
+    'animal3': 'rgb(244, 167, 89)',
+    'animal4': 'rgb(101, 164, 126)', 
+    'animal5': 'rgb(106, 205, 228)', 
+    'animal6': 'rgb(255, 218, 90)', 
+    'animal7': 'rgb(249, 193, 198)', 
+    'animal8': 'rgb(240, 104, 141)'  
 };
 
 // Данные о животных (в граммах)
 const animalData = {
     'animal1': {
-        A: { amount: 570 },
+        A: { amount: 570,},
         B: { amount: 1700},
         C: { amount: 2900}
     },
@@ -36,44 +36,30 @@ const animalData = {
         A: { amount: 5700 }
     },
     'animal4': {
-        A: { amount: 6200},
+        A: { amount: 0},
+    },
+    'animal5': {
+        A: { amount: 8200},
+        B: { amount: 8000},
+        C: { amount: 2900}
+    },
+    'animal6': {
+        A: { amount: 6020},
         B: { amount: 1800},
         C: { amount: 3000}
     },
-    'animal5': {
-        A: { amount: 8200, feedName: 'Корм А5' },
-        B: { amount: 8000, feedName: 'Корм В5' },
-        C: { amount: 2900, feedName: 'Корм С5' }
-    },
-    'animal6': {
-        A: { amount: 6020, feedName: 'Корм А6' },
-        B: { amount: 1800, feedName: 'Корм В6' },
-        C: { amount: 3000, feedName: 'Корм С6' }
-    },
     'animal7': {
-        A: { amount: 5700, feedName: 'Корм А7' },
-        B: { amount: 1700, feedName: 'Корм В7' },
-        C: { amount: 2900, feedName: 'Корм С7' }
+        A: { amount: 5700},
+        B: { amount: 1700},
+        C: { amount: 2900}
     },
     'animal8': {
-        A: { amount: 6020, feedName: 'Корм А8' },
-        B: { amount: 1800, feedName: 'Корм В8' },
-        C: { amount: 3000, feedName: 'Корм С8' },
-        C: { amount: 3000, feedName: 'Корм С9' }
+        A: { amount: 6020},
+        B: { amount: 1800},
+        C: { amount: 3000},
     }
 };
 
-// Массив с текстовыми описаниями кормов для каждого животного
-const feedDescriptions = {
-    'animal1': ['Описание корма А1', 'Описание корма В1', 'Описание корма С1'],
-    'animal2': ['Описание корма А2', 'Описание корма В2'],
-    'animal3': ['Описание корма А3'],
-    'animal4': ['Описание корма А4', 'Описание корма В4', 'Описание корма С4'],
-    'animal5': ['Описание корма А5', 'Описание корма В5', 'Описание корма С5'],
-    'animal6': ['Описание корма А6', 'Описание корма В6', 'Описание корма С6'],
-    'animal7': ['Описание корма А7', 'Описание корма В7', 'Описание корма С7'],
-    'animal8': ['Описание корма А8', 'Описание корма В8', 'Описание корма С8']
-};
 
 // Функция для смены изображения при выборе животного
 function selectAnimal(animalType) {
@@ -138,9 +124,9 @@ function updateResults(bagsOfFeedA, totalKilogramsA, feedNameA, bagsOfFeedB, tot
         <div class="itog-all">Итого: ${totalKilogramsTotal.toFixed(2)} кг (количество мешков: ${bagsOfFeedTotal})</div>
     `;
 
-    document.querySelectorAll('.text-calc')[0].innerText = `Старт ${bagsOfFeedA} мешков ${totalKilogramsA.toFixed(2)}кг`;
-    document.querySelectorAll('.text-calc')[1].innerText = `Рост ${bagsOfFeedB} мешков ${totalKilogramsB.toFixed(2)}кг`;
-    document.querySelectorAll('.text-calc')[2].innerText = `Финиш ${bagsOfFeedC} мешков ${totalKilogramsC.toFixed(2)}кг`;
+    document.querySelectorAll('.text-calc')[0].innerText = `${bagsOfFeedA} мешков ${totalKilogramsA.toFixed(2)}кг`;
+    document.querySelectorAll('.text-calc')[1].innerText = `${bagsOfFeedB} мешков ${totalKilogramsB.toFixed(2)}кг`;
+    document.querySelectorAll('.text-calc')[2].innerText = `${bagsOfFeedC} мешков ${totalKilogramsC.toFixed(2)}кг`;
 }
 
 // Функция для создания блоков с результатами
