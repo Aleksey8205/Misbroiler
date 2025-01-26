02f2e4755bc8f706aecd062523df092f8b3a3436
Aleksey8205 <Aleksey8205@yandex.ru>
2025-01-26 22:26:11 +0300

    Add files via upload

---
+++ script/slider2.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const prevButton = document.querySelector(".previrous-button");
+    const nextButton = document.querySelector(".nexts-button");
+    const slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+
+    // Функция для отображения предыдущего слайда
+    function showPrevSlide() {
+        hideCurrentSlide();
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    // Функция для отображения следующего слайда
+    function showNextSlide() {
+        hideCurrentSlide();
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    // Функция для скрытия текущего слайда
+    function hideCurrentSlide() {
+        slides[currentIndex].classList.remove("active");
+    }
+
+    // Функция для отображения слайда по индексу
+    function showSlide(index) {
+        slides[index].classList.add("active");
+    }
+
+    // Обработчик события для кнопки "предыдущий"
+    prevButton.addEventListener("click", showPrevSlide);
+
+    // Обработчик события для кнопки "следующий"
+    nextButton.addEventListener("click", showNextSlide);
+
+    // Отображение начального слайда
+    showSlide(currentIndex);
+});
